506c1a5def25e569aff0bdbff072824b606ef2e4
Chien Wen Liu <dreamuk@gmail.com>
2019-06-06 14:39:37 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <h1>Code note</h1>
+    <Nav v-bind:links="links"/>
+  </div>
+</template>
+
+<script>
+import Nav from "./components/Nav";
+export default {
+  name: "app",
+  components: {
+    Nav
+  },
+  data() {
+    return {
+      links: [{ id: 0, text: "link1" }, { id: 1, text: "link2" }]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="nav">
+    <div v-for="link in links" v-bind:key="link.id">
+      <div>{{link.text}}</div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Nav",
+  props: ["links"]
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
